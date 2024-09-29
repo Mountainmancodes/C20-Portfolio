424ba9bfdf7f95df33b6f3771e10d63dd4ba3f13
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-09-29 11:25:04 -0600

    attempt to make fixes to better align with Vite and Netlify

--- vite.config.js
+++ vite.config.js
@@ -27,16 +27,12 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@data': path.resolve(__dirname, './src/data'),
+      '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'
-    },
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/styles/variables.scss";`
-      }
     }
   },
   define: {
